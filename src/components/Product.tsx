7d37a66ae9ed57000b998655aa889bb801f2008a
Samuel Adu <theadusamuel@gmail.com>
2024-05-13 18:01:27 +0100

    style update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -10,26 +10,36 @@ type ProductProps = {
 
 function Product({ count, setCount }: ProductProps) {
   return (
-    <>
-      <div className="container product-page">
-        <Carousel />
+    <main>
+      <div className="container">
+        <div className="product-page">
+          <Carousel />
 
-        <div>
-          <h1 className="company-name">sneaker company</h1>
-          <h2 className="product-name">fall limited edition sneakers</h2>
-          <p className="product-desc">
-            These low-profile sneakers are perfect casual wear companion.
-            Featuring a durable rubber outter sole, they'll withstand everything
-            the weather can offer.
-          </p>
+          <div>
+            <h1 className="company-name">sneaker company</h1>
+            <h2 className="product-name">fall limited edition sneakers</h2>
+            <p className="product-desc">
+              These low-profile sneakers are perfect casual wear companion.
+              Featuring a durable rubber outter sole, they'll withstand
+              everything the weather can offer.
+            </p>
 
-          <div className="add-to-cart-tab">
-            <Quantity count={count} setCount={setCount} />
-            <Button />
+            <div className="price-tab">
+              <div className="new-price">
+                <p className="price">$125.00</p>
+                <p className="discount">50%</p>
+              </div>
+              <p className="old-price">$250.00</p>
+            </div>
+
+            <div className="add-to-cart-tab">
+              <Quantity count={count} setCount={setCount} />
+              <Button />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
